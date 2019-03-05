9d174a88a0590d59f202899be602bb8128e65bff
Connor Sullivan <connor.sullivan@limejump.com>
2019-03-05 18:22:18 +0000

    asset feed dashboard exercise added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+const websocketURLParams = new URLSearchParams({
+  'authorization': process.env.REACT_APP_AUTH_TOKEN,
+  'fields': "kw_inst,frequency_inst,soc,status_io_inst,status_inst,system_output"
+})
+
+const websocketURL = new URL(
+  '/ws/1.0/assets/365/live-performance?' + websocketURLParams.toString(),
+  'wss://api.limejump.com'
+)
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.handleMessage = this.handleMessage.bind(this)
+  }
+
+  handleMessage(e) {
+    console.log(e)
+  }
+
+  componentDidMount() {
+    const wss = new WebSocket(websocketURL)
+    wss.onopen = (e) => console.log('Websocket is connected.')
+    wss.onmessage = this.handleMessage
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Real time asset feed dashboard</h1>
+        <p>Using the stream based asset data via the websocket provided,
+          the candidate should build the following components and display them in a grid:
+        </p>
+        <ol>
+          <li>The top ten asset data points by value, sorted descending by value.</li>
+          <li>Average state of charge of the previous 100 data points.</li>
+          <li>Text based message stream, simply displaying the most recent asset data point.</li>
+        </ol>
       </div>
     );
   }
