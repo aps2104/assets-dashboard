a6591c0f623059685f5df3924d0f49b418840409
TomBers <tjcberman@gmail.com>
2021-10-07 14:52:06 +0100

    add test server

--- src/App.js
+++ src/App.js
@@ -2,28 +2,16 @@ import React, { useEffect } from 'react';
 
 import './App.css';
 
-const websocketURLParams = new URLSearchParams({
-  'authorization': process.env.REACT_APP_AUTH_TOKEN,
-  'fields': "kw_inst,frequency_inst,soc,status_io_inst,status_inst,system_output"
-})
-
-const websocketURL = new URL(
-  '/ws/1.0/assets/1927/live-performance?' + websocketURLParams.toString(),
-  'wss://api.limejump.com'
-)
 
 function App() {
 
   useEffect(() => {
-    const wss = new WebSocket(websocketURL)
-    wss.onopen = () => console.log('Websocket is connected.')
-    wss.onmessage = e => console.log(e);
   }, [])
 
   return (
     <div className="App">
-      <h1>Real time asset feed dashboard</h1>
-      <p>Using the stream based asset data via the websocket provided,
+      <h1>Asset feed dashboard</h1>
+      <p>Using the test server API (node server.js) 
         the candidate should build the following components and display them in a grid:
       </p>
       <ol>
